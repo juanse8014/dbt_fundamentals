54a4c04ed98aad581f8bb8122463dd6e9544d4b6
Sebastian Navarrete <sebastian.navarrete@blend360.com>
2026-02-27 03:12:53 +0000

    Adding some tests

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount/100 as amount, --convert it to dollars
+    amount,
     created as created_at,
 from {{ source('stripe', 'payment') }}
