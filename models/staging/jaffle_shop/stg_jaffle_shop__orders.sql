865304c57da5e87864fc7282f9275c56b79c2ab1
Sebastian Navarrete <sebastian.navarrete@blend360.com>
2026-03-04 14:30:48 +0000

    Adding snapshots and removing some tests

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,5 +2,6 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status as order_status
+    status as order_status,
+    _ETL_LOADED_AT as last_update
 from {{ source('jaffle_shop', 'orders') }}
