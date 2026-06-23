151a844bd43deebb60f01c3aa0273556bb25d078
Sebastian Navarrete <sebastian.navarrete@blend360.com>
2026-06-23 20:37:37 +0000

    Some tweaks in Orders model

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,20 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status as order_status,
-    _ETL_LOADED_AT as last_update
-from {{ source('jaffle_shop', 'orders') }}
+with
+source as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+staged as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        datediff(day,order_date, {{ dbt.current_timestamp() }} ) as days_since_ordered,
+        status like '%pending%' as is_status_pending,
+        case
+            when status like '%pending%' then 'placed'
+            when status like '%return%' then 'returned'
+            else status
+        end as status,
+        _ETL_LOADED_AT as last_update
+    from source
+)
+select * from staged
